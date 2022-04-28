c98f8b38e0445e26b266094f6da7862b48c42403
mollikacomputer <mollikacomputer3@gmail.com>
2022-04-29 02:00:50 +0600

    home page initialiaze

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './Components/Common/Footer/Footer';
+import Header from './Components/Common/Header/Header';
 import Home from './Components/Home/Home';
+import Inventory from './Components/Inventory/Inventory';
+import Login from './Components/Login/Login';
+import Register from './Components/Register/Register';
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>} ></Route>
+        <Route path='/home' element={<Home></Home>} ></Route>
+        <Route path='/login' element={<Login></Login>} > </Route>
+        <Route path='/register' element={<Register></Register>} ></Route>
+        <Route path='/inventory' element={<Inventory></Inventory>} ></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
-import Footer from '../Common/Footer/Footer';
-import Header from '../Common/Header/Header';
 import './Home.css';
 const Home = () => {
     return (
         <div>
-            <Header></Header>
-            <Banner></Banner>
-            <Footer></Footer>
+            <h>home</h>
+        <Banner></Banner>
+            
         </div>
     );
 };
